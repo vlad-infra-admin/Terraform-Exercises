aeca2a12b836ce1d8d9a01df3bbf46f5d128cf1e
VladWonderMan <80219009+VladWonderMan@users.noreply.github.com>
2022-02-07 14:43:00 -0800

    Add locals, apply terraform fmt

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
+}
+
+locals {
+  vm_ip       = digitalocean_droplet.web.ipv4_address
+  common_tags = ["devops", "vlad24081990_at_gmail_com"]
 }
 
 # Configure the DigitalOcean Provider
@@ -17,7 +22,7 @@ provider "digitalocean" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "eu-west-1"
+  region     = "eu-west-1"
 }
 
 data "digitalocean_ssh_key" "rebrain" {
@@ -31,25 +36,25 @@ resource "digitalocean_ssh_key" "vlad" {
 
 # Configure VM
 resource "digitalocean_droplet" "web" {
-  image  = "ubuntu-20-04-x64"
-  name   = "web-1"
-  region = "fra1"
-  size   = "s-1vcpu-1gb"
+  image    = "ubuntu-20-04-x64"
+  name     = "web-1"
+  region   = "fra1"
+  size     = "s-1vcpu-1gb"
   ssh_keys = [data.digitalocean_ssh_key.rebrain.id, digitalocean_ssh_key.vlad.fingerprint]
-  tags   = ["devops","vlad24081990_at_gmail_com"]
+  tags     = local.common_tags
 }
 
 # Configure DNS in route 53
 
 data "aws_route53_zone" "selected" {
   name = "devops.rebrain.srwx.net"
-  }
+}
 
-resource "aws_route53_record" "devops_dns"{
-zone_id = data.aws_route53_zone.selected.zone_id
-name = "vlad24081990.${data.aws_route53_zone.selected.name}"
-type = "A"
-ttl = "300"
-records = [digitalocean_droplet.web.ipv4_address]
+resource "aws_route53_record" "devops_dns" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "vlad24081990.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = "300"
+  records = [local.vm_ip]
 }
 
--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
 variable "do_token" {
-    type    = string
-    description = "Digital Ocean token"
+  type        = string
+  description = "Digital Ocean token"
 }
 
 variable "ssh_key_vlad" {
   type = string
 }
 
-variable "aws_access_key"{
+variable "aws_access_key" {
   type = string
 }
 
